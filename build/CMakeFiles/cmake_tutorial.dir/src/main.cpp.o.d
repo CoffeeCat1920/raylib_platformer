03faf09fd46094ab0e0fb9e4e5f33229dd11175b
CoffeeCat1920 <H0neyCatCoffeeGo@proton.me>
2026-08-31 15:33:09 +0500

    removing build

--- build/CMakeFiles/cmake_tutorial.dir/src/main.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/cmake_tutorial.dir/src/main.cpp.o: \
- /home/ok/Code/raylib_platform/src/main.cpp /usr/include/stdc-predef.h \
- /home/ok/Code/raylib_platform/include/Game/Public/Game.hpp \
- /usr/include/raylib.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/16/include/stdarg.h
